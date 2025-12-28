2c184d9148b394290b47d5d552f06b1bf853712f
Hritik Thakur <hritikthakur17@gmail.com>
2025-12-28 06:23:24 +0000

    challenge1/3

---
+++ script.js
@@ -0,0 +1,20 @@
+const todoInput = document.getElementById('todo-input');
+const addTaskButton = document.getElementById("add-task-btn");
+const todoList = document.getElementById("todo-list");
+
+
+let tasks = []
+
+addTaskButton.addEventListener("click", ()=>{
+    const taskTest = todoInput.value.trim()
+    if(taskTest === "") return;
+
+    const newTask = {
+        id: Date.now(),
+        text: taskTest,
+        completed: false
+    }
+    tasks.push(newTask)
+    todoInput.value = "" //clear input
+    console.log(tasks);
+} )
